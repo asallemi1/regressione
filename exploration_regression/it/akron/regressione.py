d47d6b199ed4429b999940f7be570596371beab8
alisia giulia sallemi <102531884+asallemi1@users.noreply.github.com>
2026-06-04 10:05:47 +0200

    Enhance regression metrics in train method
    Added mean absolute error, root mean squared error, and R^2 score calculations to the train method.

--- exploration_regression/it/akron/regressione.py
+++ exploration_regression/it/akron/regressione.py
@@ -1,9 +1,10 @@
 from sklearn.linear_model import ElasticNet, LinearRegression, Lasso, Ridge
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
+import numpy as np
 
 class Regressione:
     def __init__(self, X, y, metodo="elasticnet"):
@@ -36,8 +37,11 @@ class Regressione:
 
         y_pred = self.__model.predict(X_test_pca)
         mse = mean_squared_error(y_test, y_pred)
+        mae = mean_absolute_error(y_test, y_pred)
+        rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+        r2 = r2_score(y_test, y_pred)
 
-        return mse
+        return mse, mae, rmse, r2
 
 
     def splitta_dataset(self, test_size=0.2, random_state=42):
@@ -83,4 +87,3 @@ class Regressione:
         new_pca = self.__pca.transform(new_scaled)
 
         return self.__model.predict(new_pca)
-
